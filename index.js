e5fc5258f822e795ce64578d1d6cb06bb783561f
Yashpal366 <45157464+Yashpal366@users.noreply.github.com>
2024-04-22 14:59:04 +0530

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import route from "./routes/userRoute.js";
+
+const app = express();
+app.use(bodyParser.json());
+const port ="4444";
+
+mongoose.connect("mongodb+srv://yashpal:Rathore8@cluster0.ctozy2t.mongodb.net/sample").then(()=>{
+console.log("DB connected ")
+}).catch(error=>{
+    console.log("Db Error:", error)
+})
+
+app.listen(port,()=>{
+    console.log("App is listening on port:", port)
+})
+
+app.use("/", route)
